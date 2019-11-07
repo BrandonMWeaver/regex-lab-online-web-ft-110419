08c21fa685e9f370a4297e071d9945d66619c0f3
Brandon Weaver <a7xncob@gmail.com>
2019-11-07 23:35:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  return word[0].match?(/[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/\b^un/)
 end
 
 def words_five_letters_long(text)
-
+  return text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  
 end
 
 def valid_phone_number?(phone)
-
+  puts phone.match?(//)
 end
